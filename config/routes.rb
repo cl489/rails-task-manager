26129ed9483b68158c6d4ae692f3e6f79a033430
carmen lok <carmen.lok08@gmail.com>
2019-04-06 17:12:34 +0800

    task manager CRUD model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+  # read all tasks
+  # get 'tasks', to: 'tasks#index'
+  # # read one task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # create one task (2 requests, get the form, then post params)
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+
+  # # update one
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
